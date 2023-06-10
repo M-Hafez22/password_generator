3c4f937d8256f5c51497591b32a3bc3daf2dbbf3
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-06-10 18:58:47 +0300

    Get Password length

--- password_generator.py
+++ password_generator.py
@@ -1,4 +1,18 @@
-# TODO Get Password length
+# Get Password length
+minimum_password_length = int(6)
+pw_lenght = input("Password Lenght?: ")
+while True:
+    try:
+        pw_lenght = int(pw_lenght)
+        if pw_lenght < minimum_password_length:
+            print("You need more than " + str(minimum_password_length))
+            pw_lenght = input("Please enter the Password Lenght again: ")
+        else:
+            break
+    except:
+        print("Please enter numbers only")
+        pw_lenght = input("Please enter the Password Lenght again: ")
+
 # TODO Get Password characters types (Numbers, Letters, punctuation)
 # TODO Create Characters list
 # TODO shuffling the characters
