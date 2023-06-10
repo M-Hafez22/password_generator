72abc3758f0c1bcefedbe1d79c6ebf8b3d11f79e
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-06-10 18:56:25 +0300

    first commit

---
+++ password_generator.py
@@ -0,0 +1,5 @@
+# TODO Get Password length
+# TODO Get Password characters types (Numbers, Letters, punctuation)
+# TODO Create Characters list
+# TODO shuffling the characters
+# TODO Generate the password
